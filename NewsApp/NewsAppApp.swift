b49f59340888e7fdac2020e5cd3f2a02be453b51
aakash bondwal <aakashbondwal4@gmail.com>
2024-01-04 03:16:33 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Aakash  Bondwal  on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
